f0dfac561b8b5fca6823c9e71a879d8107b8a095
clarkdm <dmclark@hotmail.co.uk>
2017-08-22 17:28:16 +0100

    Rock–paper–scissors all_V_all

--- src/main/scala/RockPaperScissors.scala
+++ src/main/scala/RockPaperScissors.scala
@@ -4,7 +4,7 @@ object RockPaperScissors {
 
 
   def main(args: Array[String]): Unit = {
-    run_ai_ai()
+    run_all_V_all(3)
   }
 
 
@@ -20,6 +20,46 @@ object RockPaperScissors {
     case ('r', 'p') => 2
     case (_, _) => -1
 
+  }
+
+  def run_all_V_all(num: Int): Unit = {
+    var alive = num
+    var players: ListBuffer[(Int, Char, Boolean, Boolean, Boolean)] = new ListBuffer[(Int, Char, Boolean, Boolean, Boolean)]
+    for (x <- 1 to alive) players += Tuple5(x, ai_0(), true, false, false)
+
+    var tie: Boolean = false
+    for (x <- 0 to alive - 2; y <- x + 1 to alive - 1) {
+      println(x+" : "+y)
+
+      println(players)
+      println(tie)
+
+      if (!tie) {
+        val temp = all_rock_paper_scissors(players(x), players(y))
+        var p1 = temp._1
+        var p2 = temp._2
+        if (p1._4 & p1._5 | p2._4 & p2._5) tie = true
+        if (p1._5 == true) p1 = Tuple5(p1._1,p1._2,false,p1._4,p1._5);players(x)=p1
+        if (p2._5 == true) p2 = Tuple5(p2._1,p2._2,false,p2._4,p2._5);players(y)=p2
+
+        println(players)
+
+        println(x+" : "+y)
+      }
+    }
+println(players)
+
+  }
+
+  def all_rock_paper_scissors(player_1: (Int, Char, Boolean, Boolean, Boolean), player_2: (Int, Char, Boolean, Boolean, Boolean)): ((Int, Char, Boolean, Boolean, Boolean), (Int, Char, Boolean, Boolean, Boolean)) = (player_1, player_2) match {
+    case ((p1, x, false, win1, lose1), (p2, y, a2, win2, lose2)) => ((p1, x, false, win1, lose1), (p2, y, a2, win2, lose2))
+    case ((p1, x, a1, win1, lose1), (p2, y, false, win2, lose2)) => ((p1, x, a1, win1, lose1), (p2, y, false, win2, lose2))
+    case ((p1, x, a1, true, true), (p2, y, a2, win2, lose2)) => ((p1, x, a1, true, true), (p2, y, a2, true, true))
+    case ((p1, x, a1, win1, lose1), (p2, y, a2, true, true)) => ((p1, x, a1, true, true), (p2, y, a2, true, true))
+    case ((p1, x, a1, win1, lose1), (p2, y, a2, win2, lose2)) if rock_paper_scissors(x, y) == 0 => ((p1, x, a1, win1, lose1), (p2, y, a2, win2, lose2))
+    case ((p1, x, a1, win1, lose1), (p2, y, a2, win2, lose2)) if rock_paper_scissors(x, y) == 1 => ((p1, x, a1, true, lose1), (p2, y, a2, win2, true))
+    case ((p1, x, a1, win1, lose1), (p2, y, a2, win2, lose2)) if rock_paper_scissors(x, y) == 2 => ((p1, x, a1, win1, true), (p2, y, a2, true, lose2))
+    case ((p1, x, a1, win1, lose1), (p2, y, a2, win2, lose2)) => ((p1, x, a1, win1, lose1), (p2, y, a2, win2, lose2))
   }
 
   def run(): Unit = {
@@ -55,6 +95,10 @@ object RockPaperScissors {
     }
   }
 
+  def ai_0(): Char = {
+    get_Random()
+  }
+
   def ai_1(past_turns: ListBuffer[Char]): Char = {
     if (past_turns.size > 1) past_turns(past_turns.size - 2)
     else 'p'
@@ -65,13 +109,13 @@ object RockPaperScissors {
   }
 
   def get_total(past_turns: List[Char], sub_total: (Int, Int, Int) = (0, 0, 0)): (Int, Int, Int) = past_turns match {
-    case 'r' :: rest => get_total(rest, (sub_total._1+1,sub_total._2,sub_total._3))
-    case 'p' :: rest => get_total(rest, (sub_total._1,sub_total._2+1,sub_total._3))
-    case 's' :: rest => get_total(rest, (sub_total._1,sub_total._2,sub_total._3+1))
+    case 'r' :: rest => get_total(rest, (sub_total._1 + 1, sub_total._2, sub_total._3))
+    case 'p' :: rest => get_total(rest, (sub_total._1, sub_total._2 + 1, sub_total._3))
+    case 's' :: rest => get_total(rest, (sub_total._1, sub_total._2, sub_total._3 + 1))
     case Nil => sub_total
   }
 
-  def get_Random(): Char = scala.util.Random.nextInt(3) match {
+  def get_Random(): Char = scala.util.Random.nextInt(4) match {
     case 1 => 'p'
     case 2 => 'r'
     case 3 => 's'
