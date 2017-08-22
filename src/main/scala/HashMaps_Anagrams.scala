e75311a44866376f67b2f6e238e58309b6a1cfd3
clarkdm <dmclark@hotmail.co.uk>
2017-08-22 13:09:55 +0100

    rework HashMaps_Anagram

--- src/main/scala/HashMaps_Anagrams.scala
+++ src/main/scala/HashMaps_Anagrams.scala
@@ -1,54 +1,44 @@
 
 import scala.collection.mutable
-import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.ListBuffer
 import scala.io.Source
 
 object HashMaps_Anagrams {
 
   def main(args: Array[String]): Unit = {
-
-
     add_all(inport(filename))
     println(most_anagrams())
   }
 
   val filename = "WordList.txt"
-  var map = new mutable.HashMap[String, ArrayBuffer[String]]
+  var map = new mutable.HashMap[String, ListBuffer[String]]
 
 
   def most_anagrams(): String = {
     var max: Int = 0
-    var most: ArrayBuffer[String] = new ArrayBuffer[String]()
+    var most: ListBuffer[String] = new ListBuffer[String]()
     for (key <- map.keySet) {
       if (max < map(key).size) {
         max = map(key).size
-        most = new ArrayBuffer[String]() += key
-
+        most = new ListBuffer[String]() += key
       }
       else if (max == map(key).size) most += key
     }
-    if (most.size == 0) "No anagrams"
-    else if (most.size == 1) most(0)
-    else {
-      max = 0
-      var most_Char: ArrayBuffer[String] = new ArrayBuffer[String]()
-      for (word <- most) {
-        if (max < word.size) {
-          max = word.size
-          most_Char = new ArrayBuffer[String]() += word
-        }
-        else if (max == word.size) most_Char += word
-      }
-      if (most_Char.size == 0) "No anagrams"
-      else if (most_Char.size == 1) most(0)
-      else {
-        var temp = ""
-        most_Char.foreach(word => temp + word)
-        temp
-      }
-    }
+    print_most(most.toList, get_most(most.toList))
   }
 
+  def get_most(temp: List[String], max: Int = 0): Int = temp match {
+    case f :: rest if f.length > max => get_most(rest, f.length)
+    case _ :: rest => get_most(rest, max)
+    case Nil => max
+  }
+
+  def print_most(temp: List[String], max: Int = 0): String = (temp, max) match {
+    case (_, 0) => "No anagrams"
+    case (f :: rest, max) if f.length == max => f + " " + print_most(rest, max)
+    case (_ :: rest, max) => print_most(rest, max)
+    case (Nil, _) => ""
+  }
 
   def sort_word(word: String): String = {
     word.sorted.toLowerCase
@@ -63,7 +53,7 @@ object HashMaps_Anagrams {
     if (map.keySet.contains(sort)) {
       map(sort) += word
     }
-    else map += (sort -> (ArrayBuffer().+=(word)))
+    else map += (sort -> (ListBuffer().+=(word)))
   }
 
   def inport(filename: String): List[String] = {
